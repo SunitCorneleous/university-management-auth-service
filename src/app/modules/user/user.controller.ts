db5e280cc2839c25e4d7e3be3a675a791a1abf1f
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-06-11 14:45:55 +0600

    catch async and send response function added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,31 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = req.body;
 
     const result = await UserService.createUserToDB(user);
 
-    res.status(200).send({
+    next();
+
+    /* res.status(200).send({
+      success: true,
+      message: 'User created successfully',
+      data: result,
+    }); */
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'User created successfully',
       data: result,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 export const UserController = {
   createUser,
