d68b9a60a8667c11da75711120a2115aab9a707a
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-06-16 11:37:26 +0600

    dynamic sorting

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
@@ -8,7 +8,7 @@ import { paginationFields } from '../../../constants/pagination';
 import { IAcademicSemester } from './academicSemester.interface';
 
 const createAcademicSemester = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     const { ...academicSemesterData } = req.body;
 
     const result = await AcademicSemesterService.createSemester(
@@ -21,30 +21,24 @@ const createAcademicSemester = catchAsync(
       message: 'Academic semester created successfully',
       data: result,
     });
-
-    next();
   }
 );
 
-const getAllSemester = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const paginationOptions = pick(req.query, paginationFields);
+const getAllSemester = catchAsync(async (req: Request, res: Response) => {
+  const paginationOptions = pick(req.query, paginationFields);
 
-    const result = await AcademicSemesterService.getAllSemesters(
-      paginationOptions
-    );
+  const result = await AcademicSemesterService.getAllSemesters(
+    paginationOptions
+  );
 
-    sendResponse<IAcademicSemester[]>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Semesters retrieved successfully',
-      meta: result.meta,
-      data: result.data,
-    });
-
-    next();
-  }
-);
+  sendResponse<IAcademicSemester[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semesters retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
 export const academicSemesterController = {
   createAcademicSemester,
