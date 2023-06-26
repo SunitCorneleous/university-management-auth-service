7896476bd6b32652d3bf64ef07bec215a803246d
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-06-27 00:06:46 +0600

    browser cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,17 +4,27 @@ import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 import { AuthService } from './auth.services';
 import { ILoginUserResponse } from './auth.interface';
+import config from '../../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
 
   const result = await AuthService.loginUser(loginData);
+  const { refreshToken, ...others } = result;
+
+  // set refresh token into cookie
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+
+  res.cookie('refreshToken', refreshToken, cookieOptions);
 
   sendResponse<ILoginUserResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User logged in successfully',
-    data: result,
+    data: others,
   });
 });
 
